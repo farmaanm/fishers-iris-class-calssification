3e3568c274f78824e9c97b0f6d0e197db038497c
Farmaan <mfmohamed@students.nsbm.ac.lk>
2022-10-28 00:25:43 +0530

    Create coursework_1.m
    Loaded fisheriris data and seperated 60% for training and 40% for testing

---
+++ coursework_1.m
@@ -0,0 +1,21 @@
+% Loading fisheriris data
+load fisheriris.mat
+
+% Convert species to an index vector where 1, 2, 3 correspond to setosa, versicolor, virginica
+species = grp2idx(species);
+
+% Shuffling a Vector, seperated in to unique values
+dataset_1 = randperm(150,(150*0.6))'; % Seperating 60% of the unique values
+dataset_2 = setdiff((1:150)',dataset_1); % Seperating the reamining 40%
+
+% Getting 60% for the training dataset
+trainData = meas(dataset_1,:);
+trainTarget = species(dataset_1,:);
+
+% Getting 40% for the training dataset
+testData = meas(dataset_2,:);
+testTarget = species(dataset_2,:);
+
+% Construct a feedforward network- One layer - 10 Nodes
+net = feedforwardnet(10);
+
